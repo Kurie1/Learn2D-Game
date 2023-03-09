c3a598df9443552ecde6c1e924076d82b3ee87d0
Kurie <flykuro211@gmail.com>
2023-03-09 22:58:57 +0700

    Animator

--- 2D Game/Assets/Script/PlayerMovement.cs
+++ 2D Game/Assets/Script/PlayerMovement.cs
@@ -4,24 +4,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private int jumpHight = 14;
+    private Animator anim;
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    [SerializeField] private float jumpForce = 14f;
+    [SerializeField] private float moveSpeed = 7f;
+    private float dirX=0f;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
    private void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump")){
-            rb.velocity = new Vector2(rb.velocity.x, jumpHight);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+        UpdateAnimationState();
 
-       
+    }
+    private void UpdateAnimationState()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
+        }
     }
 }
